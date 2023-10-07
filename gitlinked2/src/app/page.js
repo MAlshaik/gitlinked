7fe59d5159311b66924f35e0607ac83a89a3c0fd
Sanya1001 <saniasinha2003@gmail.com>
2023-10-07 16:41:07 -0400

    Merge branch 'frontend' of https://github.com/MAlshaik/gitlinked into frontend

--- gitlinked2/src/app/page.js
+++ gitlinked2/src/app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
       <>
+      hi sup 
               <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <form className='w-[50%] min-w-[20rem]'>   
           <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
