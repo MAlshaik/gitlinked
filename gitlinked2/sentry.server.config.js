e398dea12c3db010d0c63321b5b4b0604ab15f90
Mohammad Alshaikhusain <workmail2776@gmail.com>
2023-10-08 19:29:07 -0400

    saslkjdafhaj

--- gitlinked2/sentry.server.config.js
+++ gitlinked2/sentry.server.config.js
@@ -4,7 +4,6 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-throw new Error("hielo sentry");
 
 Sentry.init({
   dsn: "https://a9d7f8c29a27f127683f198708a72f5d@o4506017113243648.ingest.sentry.io/4506017123991552",
@@ -13,5 +12,7 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 });
+
+Sentry.captureMessage("hilo from server");
