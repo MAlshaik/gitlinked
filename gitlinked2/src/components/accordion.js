3ff80d8869f4c938c614cc84e6d86d885f5dbd2c
Sanya1001 <saniasinha2003@gmail.com>
2023-10-08 11:13:37 -0400

    fixed accordion

--- gitlinked2/src/components/accordion.js
+++ gitlinked2/src/components/accordion.js
@@ -10,22 +10,22 @@ export default function Header(){
     </div>
   </div>
   <div className="tab">
-    <input type="checkbox" name="accordion-1" id="cb1"/>
-    <label htmlFor="cb1" className="tab__label">GitLinked</label>
-    <div className="tab__content">
-      <p>Github based project built in Hackathon</p>
-    </div>
-  </div>
-  <div className="tab">
-    <input type="checkbox" name="accordion-1" id="cb1"/>
-    <label htmlFor="cb1" className="tab__label">GitLinked</label>
-    <div className="tab__content">
-      <p>Github based project built in Hackathon</p>
-    </div>
-  </div>
-  <div className="tab">
     <input type="checkbox" name="accordion-1" id="cb2"/>
-    <label htmlFor="cb2" className="tab__label">Open multiple</label>
+    <label htmlFor="cb2" className="tab__label">GitLinked</label>
+    <div className="tab__content">
+      <p>Github based project built in Hackathon</p>
+    </div>
+  </div>
+  <div className="tab">
+    <input type="checkbox" name="accordion-1" id="cb3"/>
+    <label htmlFor="cb3" className="tab__label">GitLinked</label>
+    <div className="tab__content">
+      <p>Github based project built in Hackathon</p>
+    </div>
+  </div>
+  <div className="tab">
+    <input type="checkbox" name="accordion-1" id="cb4"/>
+    <label htmlFor="cb4" className="tab__label">Open multiple</label>
     <div className="tab__content">
     </div>
   </div>
