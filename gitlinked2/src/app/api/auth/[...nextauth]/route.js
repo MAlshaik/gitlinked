dcf8247e16e3030a0a6b4dc38812926a63f2f91e
Mohammad Alshaikhusain <workmail2776@gmail.com>
2023-10-08 09:47:23 -0400

    secret

--- gitlinked2/src/app/api/auth/[...nextauth]/route.js
+++ gitlinked2/src/app/api/auth/[...nextauth]/route.js
@@ -8,8 +8,8 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+    secret: process.env.AUTH_SECRET,
 }
 
 export const handler = NextAuth(authOptions);
