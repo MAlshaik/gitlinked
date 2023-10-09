39965513d62dbf0333fef8f7452db8087a587e37
makibrte <milicev2@msu.edu>
2023-10-08 21:34:28 -0400

    repo cleanup

--- gitlinked/src/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    // Check the request method
-    if (req.method === 'GET') {
-        res.status(200).json({ text: 'Hello from Imagine and AI Club, cool kids from MSU' });
-    } else {
-        res.status(405).end(); // Method Not Allowed
-    }
-}
